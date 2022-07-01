742daada479b788072c903f636fa4e76a3f9b526
Berzan <berzan@tutanota.com>
2022-07-01 12:01:06 +0300

    add: auto connect support

--- src/contexts/WalletContextProvider.tsx
+++ src/contexts/WalletContextProvider.tsx
@@ -19,24 +19,42 @@ export const WalletContextProvider = ({children}: {children: ReactNode}) => {
     // Connect function to connect Phantom wallet
     const connect = async () => {
         const { solana } = window as any
-        if(solana && solana.isPhantom) {
-            await solana.connect()
-            console.log("connected");
-            setConnected(true)
-        }
+
+        if(!solana && !solana?.isPhantom) return
+
+        await solana.connect()
+        console.log("connected");
+        setConnected(true)
+        
     }
 
-    // When a wallet is connected, get public key
+    // When Phantom wallet is connected, get public key
     useEffect(() => {
         async function getPublicKey() {
             const { solana } = window as any
-            if(solana && solana.isPhantom) {
-                const res = await solana.connect()
-                setPublicKey( res.publicKey.toString() )
-            }
+
+            if(!solana && !solana?.isPhantom) return
+
+            const res = await solana.connect()
+            setPublicKey( res.publicKey.toString() )
         }
         if(isConnected) getPublicKey()
     }, [isConnected])
+
+    // If Phantom wallet is connected when page gets loaded
+    // automatically connect to Phantom wallet
+    useEffect(() => {
+        async function autoConnect() {
+            const { solana } = window as any
+
+            if(!solana && !solana?.isPhantom) return
+            
+            await solana.connect({ onlyIfTrusted: true })
+            setConnected(true)
+        }
+        autoConnect()
+    }, [])
+
 
     return (
         <WalletContext.Provider value={{ publicKey, connect }}>{children}</WalletContext.Provider>
